9ea5573895effc6b1b91710d9db2ad527a6beebe
FredrikJung <fredrik.jung@hotmail.com>
2022-12-15 17:12:11 +0100

    Assignment 5 done

--- uppgift5/script.js
+++ uppgift5/script.js
@@ -1,19 +1,41 @@
 
-
-
 function generateList(){
-    let array = Array.from({
-        length: Math.floor(Math.random() * 10) + 1
-    }, () => Math.floor(Math.random() * 101));
 
-    let list = document.createElement("ol");
-    for (let i = 0; i < array.length; i++)
+    const result = document.querySelector("div");
+
+    if (result.innerHTML === "")
     {
+        let array = Array.from({
+        length: Math.floor(Math.random() * 10) + 1
+        }, () => Math.floor(Math.random() * 101));
+
+        let list = document.createElement("ol");
+        for (let i = 0; i < array.length; i++)
+        {
         let listItem = document.createElement("li");
         listItem.innerText = array[i];
         document.querySelector("#resultat").appendChild(list);
         list.appendChild(listItem);
-    }   
+        } 
+    }
+    else
+    {
+        let arrayList = document.querySelector("ol");
+        result.removeChild(arrayList);
+
+        let array = Array.from({
+        length: Math.floor(Math.random() * 10) + 1
+        }, () => Math.floor(Math.random() * 101));
+
+        let list = document.createElement("ol");
+        for (let i = 0; i < array.length; i++)
+        {
+        let listItem = document.createElement("li");
+        listItem.innerText = array[i];
+        document.querySelector("#resultat").appendChild(list);
+        list.appendChild(listItem);
+        } 
+    }
 }
 
 
