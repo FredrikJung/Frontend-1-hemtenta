b72af3fc81a966aefe45732fb66a7787ada9b9ff
FredrikJung <fredrik.jung@hotmail.com>
2022-12-15 15:41:01 +0100

    Assignment 5 done

--- uppgift5/script.js
+++ uppgift5/script.js
@@ -2,14 +2,20 @@
 
 
 function generateList(){
-    let list = document.createElement("ol");
-    document.getElementById("resultat").appendChild(list);
-    listItem = document.createElement("li") 
-    listItem.innerHTML = Array.from({
+    let array = Array.from({
         length: Math.floor(Math.random() * 10) + 1
-    }, () => Math.floor(Math.random() * 101)),
-    document.querySelector("ol").appendChild(listItem);
-    
+    }, () => Math.floor(Math.random() * 101));
+
+    console.log(array);
+
+    let list = document.createElement("ol");
+    for (let i = 0; i < array.length; i++)
+    {
+        let listItem = document.createElement("li");
+        listItem.innerText = array[i];
+        document.querySelector("#resultat").appendChild(list);
+        list.appendChild(listItem);
+    }   
 }
 
 
